7e568f260e65d14ff898dec8d3221de382534aea
Slava Khripushin <slava.cpa@yandex.ru>
2024-05-27 14:00:33 +0700

    fix: splide init

--- src/js/main.js
+++ src/js/main.js
@@ -16,64 +16,72 @@ document.addEventListener('DOMContentLoaded', function () {
     placeFocusBack: false,
   });
 
-  const slider = new Splide('.slider', {
-    arrowPath: 'M5 19.9993L35 19.9994M35 19.9994L20.8333 34.166M35 19.9994L20.8333 5.83268',
-    pagination: false,
-  });
+  if (document.querySelector('.slider')) {
+    new Splide('.slider', {
+      arrowPath: 'M5 19.9993L35 19.9994M35 19.9994L20.8333 34.166M35 19.9994L20.8333 5.83268',
+      pagination: false,
+    }).mount();
+  }
 
-  slider.mount();
+  if (document.querySelector('.gallery-slider')) {
+    new Splide('.gallery-slider', {
+      arrowPath: 'M5 19.9993L35 19.9994M35 19.9994L20.8333 34.166M35 19.9994L20.8333 5.83268',
+      type: 'loop',
+      pagination: false,
+    }).mount();
+  }
 
-  new Splide('.gallery-slider', {
-    arrowPath: 'M5 19.9993L35 19.9994M35 19.9994L20.8333 34.166M35 19.9994L20.8333 5.83268',
-    type: 'loop',
-    pagination: false,
-  }).mount();
+  if (document.querySelector('.guest-slider')) {
+    new Splide('.guest-slider', {
+      type: 'loop',
+      autoWidth: !0,
+      focus: 'center',
+      pagination: !1,
+      arrowPath: 'M5 19.9993L35 19.9994M35 19.9994L20.8333 34.166M35 19.9994L20.8333 5.83268',
+    }).mount();
+  }
 
-  new Splide('.guest-slider', {
-    type: 'loop',
-    autoWidth: !0,
-    focus: 'center',
-    pagination: !1,
-    arrowPath: 'M5 19.9993L35 19.9994M35 19.9994L20.8333 34.166M35 19.9994L20.8333 5.83268',
-  }).mount();
-
-  new Splide('.partners-big', {
-    type: 'loop',
-    autoWidth: true,
-    focus: 'center',
-    pagination: false,
-    arrows: false,
-    gap: 90,
-    autoScroll: {
-      speed: 0.2,
-      pauseOnHover: false,
-      pauseOnFocus: false,
-    },
-    breakpoints: {
-      768: {
-        gap: 50,
+  if (document.querySelector('.partners-big')) {
+    new Splide('.partners-big', {
+      type: 'loop',
+      autoWidth: true,
+      focus: 'center',
+      pagination: false,
+      arrows: false,
+      gap: 90,
+      autoScroll: {
+        speed: 0.2,
+        pauseOnHover: false,
+        pauseOnFocus: false,
       },
-    },
-  }).mount(window.splide.Extensions);
-
-  new Splide('.partners-small', {
-    type: 'loop',
-    autoWidth: true,
-    focus: 'center',
-    pagination: false,
-    arrows: false,
-    gap: 90,
-    autoScroll: {
-      speed: -0.2,
-      pauseOnHover: false,
-      pauseOnFocus: false,
-    },
-    breakpoints: {
-      768: {
-        gap: 50,
+      breakpoints: {
+        768: {
+          gap: 50,
+        },
       },
-    },
-  }).mount(window.splide.Extensions);
+    }).mount(window.splide.Extensions);
+  }
+
+  if (document.querySelector('.partners-small')) {
+    new Splide('.partners-small', {
+      type: 'loop',
+      autoWidth: true,
+      focus: 'center',
+      pagination: false,
+      arrows: false,
+      gap: 90,
+      autoScroll: {
+        speed: -0.2,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+      },
+      breakpoints: {
+        768: {
+          gap: 50,
+        },
+      },
+    }).mount(window.splide.Extensions);
+  }
 
   const body = document.querySelector('body');
   const header = document.querySelector('.header');
